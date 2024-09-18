a81d13597f28f21920c502557514342995ea1172
William Wun <williamwun@hotmail.com>
2024-09-17 17:22:57 -0700

    project finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <h1 className="text-4xl font-extrabold text-gray-700 text-center">
+            Cuánto <span className="text-indigo-600">dinero</span> necesitas?
+        </h1>
+    )
+}
